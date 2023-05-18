5e9ad175c4c18d05bb172eb7379b9e6ba9f92267
Miguel Alejandro Correa (PWC) <miguelalejandro.correa.avila@pwc.com>
2023-05-18 12:02:17 +0100

    Initial Commit

---
+++ Fotos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fotos
+//
+//  Created by Miguel Alejandro Correa Avila on 18/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fotos/FotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotosApp.swift
+//  Fotos
+//
+//  Created by Miguel Alejandro Correa Avila on 18/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
